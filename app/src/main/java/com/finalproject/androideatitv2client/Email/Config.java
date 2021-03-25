fbe74a0263fdd822b93f9e3dbfa441f179475dc4
unknown <watcharavuth2000@gmail.com>
2021-03-25 20:35:50 +0700

    report send email

---
+++ app/src/main/java/com/finalproject/androideatitv2client/Email/Config.java
@@ -0,0 +1,8 @@
+package com.finalproject.androideatitv2client.Email;
+
+public class Config {
+    //public static final String EMAIL ="exe.app.report";
+    //public static final String PASSWORD ="admin101admin";
+    public static final String EMAIL ="watcharavuth2000";
+    public static final String PASSWORD ="Er94y8yu72";
+}
